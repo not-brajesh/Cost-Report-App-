26542aef697671d7c2c5a7a1ce2c793c789dbf67
not-brajesh <kbrajesh5688@gmail.com>
2026-05-01 03:53:54 +0530

    Add PWA support with service worker and manifest for offline usage

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'cost-report-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/index-2b8768da.js',
+  '/assets/index-0840cf00.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
